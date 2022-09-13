38e9bf9f9639523436110b0f93b55f9dbbdb1e89
laubento <lauiribarrenn@gmail.com>
2022-09-13 16:28:36 -0300

    primer commit

---
+++ src/models/Dog.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    Peso: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    anoDeVida:{
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+};
